42ab9f0dd715c3e44529fe26c6ce4721611c7a01
Maxw78 <maxwallen78@gmail.com>
2020-04-29 18:54:03 +0100

    Added Appointment class atributes and constraints and also added Controller and Views for Appointment Domain. All Domain Classes, constraints, controllers and views created.

--- appointmentSystem/grails-app/domain/com/mallen/Appointment.groovy
+++ appointmentSystem/grails-app/domain/com/mallen/Appointment.groovy
@@ -2,6 +2,17 @@ package com.mallen
 
 class Appointment {
 
+	Date appDate
+	String appTime
+	int appDuration
+	String roomNumber
+
     static constraints = {
+
+	appDate nullable: false, blank: false
+	appTime nullable: false, blank: false, maxSize: 20
+	appDuration nullable: false, blank: false, max: 120
+	roomNumber nullable: false, blank: false, maxSize: 10
+
     }
 }
